c9ebc231c6ed4f81bea5f985ecc304b7e9433d3e
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-01-03 15:01:37 -0300

    Se agregó comentarios

---
+++ 02 - Express2/users.js
@@ -0,0 +1,13 @@
+const arrayUsers = () => {
+  const users = [
+    { id: 1, Nombre: 'Marcos', Apellido: 'Parella' },
+    { id: 2, Nombre: 'Johan', Apellido: 'Posada' },
+    { id: 3, Nombre: 'Diana', Apellido: 'Atobe' },
+    { id: 4, Nombre: 'Renzo', Apellido: 'Trabucco' },
+  ];
+  return users;
+};
+
+module.exports = {
+  arrayUsers,
+};
